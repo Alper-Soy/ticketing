fb1c2481f6bb93135cecb80cdd0957f56de2c613
Alper Soy <51034169+Alper-Soy@users.noreply.github.com>
2021-02-01 15:28:09 +0300

    Adding Session Support

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,6 +3,7 @@ import 'express-async-errors';
 import { json } from 'body-parser';
 import morgan from 'morgan';
 import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
 
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
@@ -12,7 +13,15 @@ import { errorHandler } from './middlewares/error-handler';
 import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
+
+app.set('trust proxy', true);
 app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
 app.use(morgan('dev'));
 
 app.use(currentUserRouter);
