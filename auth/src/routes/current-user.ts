bbdd188d79e122ae0c1491e26e763adf71b1c01a
Alper Soy <51034169+Alper-Soy@users.noreply.github.com>
2021-02-03 17:28:52 +0300

    Returning the Current User

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,20 @@
 import express, { Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
 router.get('/api/users/currentuser', (req: Request, res: Response) => {
-  res.send('Hi there!');
+  // if(!req.session || !req.session.jwt) === if(!req.session?.jwt)
+  if (!req.session?.jwt) {
+    return res.send({ currentuser: null });
+  }
+
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+    res.send({ currentuser: payload });
+  } catch (err) {
+    res.send({ currentuser: null });
+  }
 });
 
 export { router as currentUserRouter };
